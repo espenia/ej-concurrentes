bd18031a133392429e81f75ddeea60dd79be42b9
Esteban Peña <efpenia@fi.uba.ar>
2023-09-24 20:45:30 -0300

    con objetos, pero medio trucho

--- filosofos/src/main.rs
+++ filosofos/src/main.rs
@@ -1,5 +1,8 @@
 use std::{thread, sync::{Mutex, Arc}};
+use std::ops::Deref;
 use std_semaphore::Semaphore;
+use crate::fork::fork::Fork;
+use crate::philosopher::philosopher::Philosopher;
 // use crate::fork::fork::Fork;
 // use crate::philosopher::philosopher::Philosopher;
 
@@ -9,31 +12,36 @@ mod philosopher;
 
 fn main() {
     let mut available_forks = vec![
-        Arc::new(Semaphore::new(1)),
-        Arc::new(Semaphore::new(1)),
-        Arc::new(Semaphore::new(1)),
-        Arc::new(Semaphore::new(1)),
-        Arc::new(Semaphore::new(1))
+        Arc::new(Mutex::new(Fork::new())),
+        Arc::new(Mutex::new(Fork::new())),
+        Arc::new(Mutex::new(Fork::new())),
+        Arc::new(Mutex::new(Fork::new())),
+        Arc::new(Mutex::new(Fork::new()))
     ];
     let mut philosophers = vec![
-        1,2,3,4,5
+        Philosopher::new("Aristotle".to_string()),
+        Philosopher::new("Kant".to_string()),
+        Philosopher::new("Spinoza".to_string()),
+        Philosopher::new("Marx".to_string()),
+        Philosopher::new("Russell".to_string()),
     ];
-    let threads = philosophers.into_iter().enumerate().map(|(i, philosopher)| {
-        println!("Philosopher {} is thinking", philosopher);
+    let threads = philosophers.into_iter().enumerate().map(|(i, mut philosopher)| {
+        println!("Philosopher {:?} is thinking", philosopher.name);
         let left_fork = available_forks[i].clone();
         let right_fork = available_forks[(i + 1) % available_forks.len()].clone();
+        
         thread::spawn(move || {
-            println!("Philosopher {} picking up left fork {}", philosopher, i);
-            left_fork.acquire();
-            println!("Philosopher {} picked up left fork {}", philosopher, i);
-            println!("Philosopher {} picking up right fork {}", philosopher, (i + 1) % 5);
-            right_fork.acquire();
-            println!("Philosopher {} picked up right fork {}", philosopher, (i + 1) % 5);
-            println!("Philosopher {} is eating", philosopher);
+            println!("Philosopher {:?} picking up left fork {}", philosopher.name, i);
+            philosopher.grab_left_fork(left_fork).unwrap();
+            println!("Philosopher {} picked up left fork {}", philosopher.name, i);
+            println!("Philosopher {} picking up right fork {}", philosopher.name, (i + 1) % 5);
+            philosopher.grab_right_fork(right_fork.try_lock().unwrap().deref().to_owned()).unwrap();
+            println!("Philosopher {} picked up right fork {}", philosopher.name, (i + 1) % 5);
+            println!("Philosopher {} is eating", philosopher.name);
             thread::sleep(std::time::Duration::from_secs(3));
-            println!("Philosopher {} is done eating", philosopher);
-            left_fork.release();
-            right_fork.release();
+            println!("Philosopher {} is done eating", philosopher.name);
+            philosopher.drop_left_fork().expect("TODO: panic message");
+            philosopher.drop_right_fork().expect("TODO: panic message");
         })
     }).collect::<Vec<_>>();
     threads.into_iter().flat_map(|t| t.join()).for_each(drop);
--- filosofos/src/philosopher/philosopher.rs
+++ filosofos/src/philosopher/philosopher.rs
@@ -1,89 +1,89 @@
-// use std::sync::MutexGuard;
-// use crate::fork::fork::{Fork};
-//
-// #[derive(Debug)]
-// pub struct Philosopher {
-//     pub(crate) left_fork: Option<i32>,
-//     pub(crate) right_fork: Option<i32>,
-//     eating: bool,
-//     thinking: bool,
-// }
-//
-// const ALREADY_HAVE_LEFT_FORK: String = "already holding a fork in the left hand".parse().unwrap();
-// const ALREADY_HAVE_RIGHT_FORK: String = "already holding a fork in the right hand".parse().unwrap();
-// const DONT_HAVE_BOTH_FORKS: String = "don't have both forks".parse().unwrap();
-//
-// impl Philosopher {
-//     pub fn new() -> Philosopher {
-//         Philosopher {
-//             left_fork: None,
-//             right_fork: None,
-//             eating: false,
-//             thinking: true,
-//         }
-//     }
-//
-//     // pub fn grab_left_fork(&mut self, mut fork: Fork) -> Result<bool, String> {
-//     //     if self.left_fork.is_none() {
-//     //         return match fork.hold() {
-//     //             Ok(..) => {
-//     //                 self.left_fork = Option::from(fork);
-//     //                 Ok(true)
-//     //             },
-//     //             Err(e) => {
-//     //                 Err(e)
-//     //             }
-//     //         }
-//     //     } else {
-//     //         Err(ALREADY_HAVE_LEFT_FORK)
-//     //     }
-//     // }
-//     //
-//     // pub fn grab_right_fork(&mut self, mut fork: Fork) -> Result<bool, String> {
-//     //     if self.right_fork.is_none() {
-//     //         return match fork.hold() {
-//     //             Ok(..) => {
-//     //                 self.right_fork = Option::from(fork);
-//     //                 Ok(true)
-//     //             },
-//     //             Err(e) => {
-//     //                 Err(e)
-//     //             }
-//     //         }
-//     //     } else {
-//     //         Err(ALREADY_HAVE_RIGHT_FORK)
-//     //     }
-//     // }
-//
-//     pub fn eat(&mut self) -> Result<bool, String> {
-//         if self.left_fork.is_some() && self.right_fork.is_some() {
-//             println!("Eating");
-//             Ok(true)
-//         } else {
-//             println!("Can't eat");
-//             Err(DONT_HAVE_BOTH_FORKS)
-//         }
-//     }
-//
-//     pub fn drop_left_fork(&mut self) -> Result<Fork, String> {
-//         if self.left_fork.is_some() {
-//             self.left_fork.release();
-//             self.left_fork = None;
-//             Ok(self.left_fork.unwrap())
-//         } else {
-//             Err("don't have a left fork".parse().unwrap())
-//         }
-//     }
-//
-//     pub fn drop_right_fork(&mut self) -> Result<Fork, String> {
-//         if self.right_fork.is_some() {
-//             self.right_fork = None;
-//             self.right_fork.release();
-//             Ok(self.right_fork.unwrap())
-//         } else {
-//             Err("don't have a right fork".parse().unwrap())
-//         }
-//     }
-//
-//
-// }
+use std::sync::{Arc, Mutex};
+use crate::fork::fork::{Fork};
+
+#[derive(Debug)]
+pub struct Philosopher {
+    pub(crate) left_fork: Option<Fork>,
+    pub(crate) right_fork: Option<Fork>,
+    eating: bool,
+    thinking: bool,
+    pub(crate) name: String,
+}
+
+const ALREADY_HAVE_LEFT_FORK: &str = "already holding a fork in the left hand";
+const ALREADY_HAVE_RIGHT_FORK: &str = "already holding a fork in the right hand";
+const DONT_HAVE_BOTH_FORKS: &str = "don't have both forks";
+
+impl Philosopher {
+    pub fn new(name: String) -> Philosopher {
+        Philosopher {
+            left_fork: None,
+            right_fork: None,
+            eating: false,
+            thinking: true,
+            name
+        }
+    }
+
+    pub fn grab_left_fork(&mut self, mut fork: Arc<Mutex<Fork>>) -> Result<bool, String> {
+        if self.left_fork.is_none() {
+            return match fork.hold() {
+                Ok(..) => {
+                    self.left_fork = Option::from(fork.try_lock().unwrap().clone());
+                    Ok(true)
+                },
+                Err(e) => {
+                    Err(e)
+                }
+            }
+        } else {
+            Err(ALREADY_HAVE_LEFT_FORK.parse().unwrap())
+        }
+    }
+
+    pub fn grab_right_fork(&mut self, mut fork: Fork) -> Result<bool, String> {
+        if self.right_fork.is_none() {
+            return match fork.hold() {
+                Ok(..) => {
+                    self.right_fork = Option::from(fork);
+                    Ok(true)
+                },
+                Err(e) => {
+                    Err(e)
+                }
+            }
+        } else {
+            Err(ALREADY_HAVE_RIGHT_FORK.parse().unwrap())
+        }
+    }
+
+    pub fn eat(&mut self) -> Result<bool, String> {
+        if self.left_fork.is_some() && self.right_fork.is_some() {
+            println!("Eating");
+            Ok(true)
+        } else {
+            println!("Can't eat");
+            Err(DONT_HAVE_BOTH_FORKS.parse().unwrap())
+        }
+    }
+
+    pub fn drop_left_fork(&mut self) -> Result<Fork, String> {
+        if self.left_fork.is_some() {
+            self.left_fork.unwrap().release();
+            Ok(self.left_fork.unwrap())
+        } else {
+            Err("don't have a left fork".parse().unwrap())
+        }
+    }
+
+    pub fn drop_right_fork(&mut self) -> Result<Fork, String> {
+        if self.right_fork.is_some() {
+            self.right_fork.unwrap().release();
+            Ok(self.right_fork.unwrap())
+        } else {
+            Err("don't have a right fork".parse().unwrap())
+        }
+    }
+
+
+}
