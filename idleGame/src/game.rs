421d107ff6a1fde05011281771ac27bea63135e2
Esteban Peña <efpenia@fi.uba.ar>
2023-09-24 13:22:08 -0300

    idle game working

--- idleGame/src/game.rs
+++ idleGame/src/game.rs
@@ -1,13 +1,13 @@
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, MutexGuard, TryLockResult};
 use std::thread;
 use std::time::Duration;
 use std::io::{stdin, stdout, Write};
+use std_semaphore::Semaphore;
 
 use crate::tech::tech::{BaseTech, Farm, Mine, Techs, Tree};
 use crate::user::user::User;
 
 pub fn idle(user_ref: &mut Arc<Mutex<User>>) {
-    thread::sleep(Duration::from_secs(1));
     let mut user = user_ref.lock().unwrap();
     let techs = user.get_tech();
     let mut gained = techs.0.len() as u64 * BaseTech::new().produce as u64;
@@ -15,13 +15,11 @@ pub fn idle(user_ref: &mut Arc<Mutex<User>>) {
     gained += techs.2.len() as u64 * Farm::new().produce as u64;
     gained += techs.3.len() as u64 * Mine::new().produce as u64;
     user.add_gold(gained);
-    println!("Gained {} gold", gained);
-    println!("Gold: {} | ", user.get_gold());
+    //println!("Gained {} gold", gained);
+    //println!("Gold: {} | ", user.get_gold());
 }
 
 pub fn store(user_ref: &mut Arc<Mutex<User>>) {
-    let mut user = user_ref.lock().unwrap();
-
     let mut s=String::new();
     print!("Do you want to buy: \
         Current Gold: {} \
@@ -30,15 +28,17 @@ pub fn store(user_ref: &mut Arc<Mutex<User>>) {
         \n3. Farm Tech \
         \n4. Mine Tech \
         \n5. Exit \
-        \n> ", user.get_gold());
+        \n> ", user_ref.try_lock().unwrap().get_gold());
     let _=stdout().flush();
     stdin().read_line(&mut s).expect("Did not enter a correct string");
     let s = s.trim();
+    let mut user = user_ref.try_lock().unwrap();
     match s {
         "1" => {
             if user.get_gold() >= BaseTech::new().cost as u64 {
                 user.add_base_tech(BaseTech::new());
                 user.remove_gold(BaseTech::new().cost as u64);
+                println!("Bought Base Tech, now have {} gold, and {} base techs", user.get_gold(), user.get_tech().0.len());
             } else {
                 println!("Not enough gold");
             }
@@ -47,6 +47,7 @@ pub fn store(user_ref: &mut Arc<Mutex<User>>) {
             if user.get_gold() >= Tree::new().cost as u64 {
                 user.add_tree_tech(Tree::new());
                 user.remove_gold(Tree::new().cost as u64);
+                println!("Bought Tree Tech, now have {} gold, and {} tree techs", user.get_gold(), user.get_tech().1.len());
             } else {
                 println!("Not enough gold");
             }
@@ -55,6 +56,7 @@ pub fn store(user_ref: &mut Arc<Mutex<User>>) {
             if user.get_gold() >= Farm::new().cost as u64 {
                 user.add_farm_tech(Farm::new());
                 user.remove_gold(Farm::new().cost as u64);
+                println!("Bought Farm Tech, now have {} gold, and {} farm techs", user.get_gold(), user.get_tech().2.len());
             } else {
                 println!("Not enough gold");
             }
@@ -63,11 +65,10 @@ pub fn store(user_ref: &mut Arc<Mutex<User>>) {
             if user.get_gold() >= Mine::new().cost as u64 {
                 user.add_mine_tech(Mine::new());
                 user.remove_gold(Mine::new().cost as u64);
+                println!("Bought Mine Tech, now have {} gold, and {} mine techs", user.get_gold(), user.get_tech().3.len());
             } else {
                 println!("Not enough gold");
             }
         },
         &_ => {} }
-    thread::sleep(Duration::from_secs(3));
-
 }
--- idleGame/src/main.rs
+++ idleGame/src/main.rs
@@ -1,5 +1,6 @@
 use std::{thread, sync::{Mutex, Arc}};
-
+use std::time::Duration;
+use std_semaphore::Semaphore;
 use user::user::User;
 
 mod tech;
@@ -8,20 +9,25 @@ mod game;
 
 
 fn main() {
+    // let user_status = Arc::new(Semaphore::new(0));
+    // let user_idling = user_status.clone();
+    // let user_buying = user_status.clone();
     let user = Arc::new(Mutex::new(User::new()));
-    let mut user_ref = Arc::clone(&user);
-    let mut store_ref = Arc::clone(&user);
-    thread::spawn(move || {
+    let mut user_idle_ref = Arc::clone(&user);
+    let mut user_store_ref = Arc::clone(&user);
+    let t1 = thread::spawn(move || {
         loop {
-            game::idle(&mut user_ref)
+            game::idle(&mut user_idle_ref);
+            thread::sleep(Duration::from_millis(100));
         }
     });
-    thread::spawn(move || {
+    let t2 = thread::spawn(move || {
         loop {
-            game::store(&mut store_ref)
+            game::store(&mut user_store_ref);
+            thread::sleep(Duration::from_secs(3));
         }
     });
-
-    thread::sleep(std::time::Duration::from_secs(10));
+    t1.join().unwrap();
+    t2.join().unwrap();
 }
 
