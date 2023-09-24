d5bfddfe899a1173f7ec9b35dd40a529cc2467cd
Esteban Peña <efpenia@fi.uba.ar>
2023-09-24 20:12:57 -0300

    semaphore

--- filosofos/src/fork/fork.rs
+++ filosofos/src/fork/fork.rs
@@ -1,33 +1,33 @@
-use uuid::Uuid;
-
-#[derive(Debug, Clone)]
-pub struct Fork {
-    pub(crate) id: Uuid,
-    pub(crate) being_held: bool,
-}
-
-const ALREADY_BEING_HOLD: String = "Fork is already being held".parse().unwrap();
-
-impl  Fork {
-    pub fn new() -> Fork {
-        Fork {
-            id: Uuid::new_v4(),
-            being_held: false,
-        }
-    }
-
-    pub fn hold(&mut self) -> Result<(), String> {
-        if self.being_held {
-            return Err(ALREADY_BEING_HOLD.clone());
-        }
-        self.being_held = true;
-        Ok(())
-    }
-
-    pub fn release(&mut self) {
-        self.being_held = false;
-    }
-}
-
-
-    
+// use uuid::Uuid;
+//
+// #[derive(Debug, Clone)]
+// pub struct Fork {
+//     pub(crate) id: Uuid,
+//     pub(crate) being_held: bool,
+// }
+//
+// const ALREADY_BEING_HOLD: String = "Fork is already being held".parse().unwrap();
+//
+// impl  Fork {
+//     pub fn new() -> Fork {
+//         Fork {
+//             id: Uuid::new_v4(),
+//             being_held: false,
+//         }
+//     }
+//
+//     pub fn hold(&mut self) -> Result<(), String> {
+//         if self.being_held {
+//             return Err(ALREADY_BEING_HOLD.clone());
+//         }
+//         self.being_held = true;
+//         Ok(())
+//     }
+//
+//     pub fn release(&mut self) {
+//         self.being_held = false;
+//     }
+// }
+//
+//
+//
--- filosofos/src/main.rs
+++ filosofos/src/main.rs
@@ -1,6 +1,7 @@
 use std::{thread, sync::{Mutex, Arc}};
-use crate::fork::fork::Fork;
-use crate::philosopher::philosopher::Philosopher;
+use std_semaphore::Semaphore;
+// use crate::fork::fork::Fork;
+// use crate::philosopher::philosopher::Philosopher;
 
 mod fork;
 mod philosopher;
@@ -8,30 +9,33 @@ mod philosopher;
 
 fn main() {
     let mut available_forks = vec![
-        Arc::new(Mutex::new(Fork::new())),
-        Arc::new(Mutex::new(Fork::new())),
-        Arc::new(Mutex::new(Fork::new())),
-        Arc::new(Mutex::new(Fork::new())),
-        Arc::new(Mutex::new(Fork::new()))
+        Arc::new(Semaphore::new(1)),
+        Arc::new(Semaphore::new(1)),
+        Arc::new(Semaphore::new(1)),
+        Arc::new(Semaphore::new(1)),
+        Arc::new(Semaphore::new(1))
     ];
     let mut philosophers = vec![
-        Arc::new(Mutex::new(Philosopher::new())),
-        Arc::new(Mutex::new(Philosopher::new())),
-        Arc::new(Mutex::new(Philosopher::new())),
-        Arc::new(Mutex::new(Philosopher::new())),
-        Arc::new(Mutex::new(Philosopher::new()))
+        1,2,3,4,5
     ];
-    // esto lo tiro co-pilot ni idea jaja
-    let threads = philosophers.iter_mut().enumerate().map(|(i, philosopher)| {
+    let threads = philosophers.into_iter().enumerate().map(|(i, philosopher)| {
+        println!("Philosopher {} is thinking", philosopher);
         let left_fork = available_forks[i].clone();
         let right_fork = available_forks[(i + 1) % available_forks.len()].clone();
-        let philosopher = philosopher.clone();
         thread::spawn(move || {
-            let mut philosopher = philosopher.lock().unwrap();
-            philosopher.left_fork = Some(left_fork);
-            philosopher.right_fork = Some(right_fork);
-            philosopher.eat();
+            println!("Philosopher {} picking up left fork {}", philosopher, i);
+            left_fork.acquire();
+            println!("Philosopher {} picked up left fork {}", philosopher, i);
+            println!("Philosopher {} picking up right fork {}", philosopher, (i + 1) % 5);
+            right_fork.acquire();
+            println!("Philosopher {} picked up right fork {}", philosopher, (i + 1) % 5);
+            println!("Philosopher {} is eating", philosopher);
+            thread::sleep(std::time::Duration::from_secs(3));
+            println!("Philosopher {} is done eating", philosopher);
+            left_fork.release();
+            right_fork.release();
         })
     }).collect::<Vec<_>>();
+    threads.into_iter().flat_map(|t| t.join()).for_each(drop);
 }
 
